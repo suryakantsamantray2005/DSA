edfc0b917f6f03b1255250ab603e74a12ca74055
Suryakant <suryakantsamantray2005@gmail.com>
2026-06-27 22:30:01 +0530

    Time: 0 ms (100%), Space: 16.3 MB (75.39%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.py
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.py
@@ -12,13 +12,7 @@ class Solution:
             mid=(low+high)//2
             if nums[mid]!=nums[mid-1] and nums[mid]!=nums[mid+1]:
                 return nums[mid]
-            elif mid%2==1:
-                if nums[mid]==nums[mid-1]:
-                    low=mid+1
-                else:
-                    high=mid-1
+            elif (mid%2==1 and nums[mid]==nums[mid-1]) or (mid%2==0 and nums[mid]==nums[mid+1]):
+                low=mid+1
             else:
-                if nums[mid]==nums[mid-1]:
-                    high=mid-1
-                else:
-                    low=mid+1
+                high=mid-1
