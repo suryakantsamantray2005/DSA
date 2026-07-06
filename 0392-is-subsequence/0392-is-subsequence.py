2e93455e1e6cf366e30565226700a5aa0e3fd362
Suryakant <suryakantsamantray2005@gmail.com>
2026-07-06 23:01:50 +0530

    Time: 3 ms (39.18%), Space: 12.4 MB (46.42%) - LeetHub

---
+++ 0392-is-subsequence/0392-is-subsequence.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def isSubsequence(self, s, t):
+        count=0
+        i,j=0,0
+        while i<len(s) and j<len(t):
+            if s[i]==t[j]:
+                i+=1
+                j+=1
+                count+=1
+            else:
+                j+=1
+        if count==len(s):
+            return True
+        else:
+            return False
