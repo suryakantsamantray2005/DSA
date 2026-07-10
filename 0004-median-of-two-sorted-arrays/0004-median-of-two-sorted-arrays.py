0693479f46df53cece7ae432d7b1b314b76f1618
Suryakant <suryakantsamantray2005@gmail.com>
2026-07-10 18:50:47 +0530

    Time: 6 ms (26.76%), Space: 12.6 MB (50.22%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
@@ -1,21 +1,39 @@
 class Solution:
     def findMedianSortedArrays(self, nums1, nums2):
-        i,j=0,0
-        L=[]
-        while i<len(nums1) and j<len(nums2):
-            if nums1[i]<=nums2[j]:
-                L.append(nums1[i])
-                i+=1
-            else:
-                L.append(nums2[j])
-                j+=1
-        while j<len(nums2):
-            L.append(nums2[j])
-            j+=1
-        while i<len(nums1):
-            L.append(nums1[i])
-            i+=1
-        if len(L)%2==0:
-            return float(L[(len(L)-1)//2]+L[(len(L)//2)])/2
+        if len(nums1)>len(nums2):
+            nums1,nums2=nums2,nums1
+        length=len(nums1)+len(nums2)
+        if length%2==0:
+            low=0
+            high=len(nums1)
+            partition=length//2
+            while low<=high:
+                cut1=(low+high)//2
+                cut2=partition-cut1
+                left1=float('-inf') if cut1==0 else nums1[cut1-1]
+                right1=float('inf') if cut1==len(nums1) else nums1[cut1]
+                left2=float('-inf') if cut2==0 else nums2[cut2-1]
+                right2=float('inf') if cut2==len(nums2) else nums2[cut2]
+                if left1<=right2 and left2<=right1:
+                    return float(max(left1,left2)+min(right1,right2))/2
+                elif left1>right2:
+                    high=cut1-1
+                else:
+                    low=cut1+1
         else:
-            return float(L[len(L)//2])
+            low=0
+            high=len(nums1)
+            partition=(length+1)//2
+            while low<=high:
+                cut1=(low+high)//2
+                cut2=partition-cut1
+                left1=float('-inf') if cut1==0 else nums1[cut1-1]
+                right1=float('inf') if cut1==len(nums1) else nums1[cut1]
+                left2=float('-inf') if cut2==0 else nums2[cut2-1]
+                right2=float('inf') if cut2==len(nums2) else nums2[cut2]
+                if left1<=right2 and left2<=right1:
+                    return float(max(left1,left2))
+                elif left1>right2:
+                    high=cut1-1
+                else:
+                    low=cut1+1
