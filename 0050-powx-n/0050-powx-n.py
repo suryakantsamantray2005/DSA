0222a49a3ebd788493002e0b7c16ad9e8ffcfeb6
Suryakant <suryakantsamantray2005@gmail.com>
2026-08-26 23:33:57 +0530

    Time: 0 ms (100%), Space: 12.5 MB (28.42%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.py
@@ -0,0 +1,18 @@
+class Solution:
+    def myPow(self, x, n):
+        ans=1
+        k=n
+        if n<0:
+            n=-n
+        while n>0:
+            if n%2==0:
+                x=x*x
+                n=n//2
+            else:
+                ans=ans*x
+                n=n-1
+        if k<0:
+            return 1/(ans)
+        else:
+            return ans
+        
