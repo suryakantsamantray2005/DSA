2fb99fe63d62d8b943976ddffe54eec5dfcfc0b1
Suryakant <suryakantsamantray2005@gmail.com>
2026-09-01 05:37:26 +0530

    Time: 239 ms (52.73%), Space: 92.4 MB (52.73%) - LeetHub

--- 2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points.py
+++ 2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points.py
@@ -3,32 +3,31 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-class Solution:
+class Solution(object):
     def nodesBetweenCriticalPoints(self, head):
-        prev=head
-        temp=head.next
-        i=0
-        critical_points=0
-        min_distance=0
-        max_distance=0
-        first_critical=0
-        previous_critical=0
-        while temp is not None:
-            i+=1
-            if prev is not None and temp.next is not None:
-                if (temp.val>prev.val and temp.val>temp.next.val) or (temp.val<prev.val and temp.val<temp.next.val):
-                    critical_points+=1
-                    if critical_points==1:
-                        first_critical=i
-                        previous_critical=i
-                    if critical_points==2:
-                        min_distance=i-first_critical
-                    if critical_points>1:
-                        max_distance=i-first_critical
-                        min_distance=min(min_distance,i-previous_critical)
-                    previous_critical=i
-            temp=temp.next
-            prev=prev.next
-        if critical_points<2:
+       temp=head.next
+       prev=head
+       critical_points=0
+       ans=[]
+       i=0
+       while temp is not None:
+        i+=1
+        if prev is not None and temp.next is not None:
+            if (temp.val>prev.val and temp.val>temp.next.val) or (temp.val<prev.val and temp.val<temp.next.val):
+                critical_points+=1
+                ans.append(i)
+        temp=temp.next
+        prev=prev.next
+       if critical_points<2:
             return [-1,-1]
-        return [min_distance,max_distance]
+       minimum=float('inf')
+       j=0
+       k=1
+       while k<len(ans):
+           l=ans[k]-ans[j]
+           if l<minimum:
+              minimum=l
+           j+=1
+           k+=1
+       return [minimum,ans[-1]-ans[0]]
+        
